782c75abe3220d50e888fc9a4706eac30da28d24
DESKTOP-DN5HQBI\gyuri <gyurivukman@gmail.com>
2018-04-09 17:39:29 +0200

    Initial-commit #2

---
+++ Backend/src/main/java/elte/projeszk/projeszk_receptek/ProjeszkReceptekApplication.java
@@ -0,0 +1,12 @@
+package elte.projeszk.projeszk_receptek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjeszkReceptekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjeszkReceptekApplication.class, args);
+	}
+}
---
+++ Backend/src/test/java/elte/projeszk/projeszk_receptek/ProjeszkReceptekApplicationTests.java
@@ -0,0 +1,16 @@
+package elte.projeszk.projeszk_receptek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjeszkReceptekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
